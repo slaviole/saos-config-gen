511a2278a64517695ad9b1f787feb5bc5324bd3a
serge.laviolette68@gmail.com <serge.laviolette68@gmail.com>
2025-10-16 00:58:04 +0000

    Created out_of_band_mgmt; Edited settings and theme/parameters.yaml; Deleted Form1

---
+++ client_code/out_of_band_mgmt/__init__.py
@@ -0,0 +1,36 @@
+from ._anvil_designer import out_of_band_mgmtTemplate
+from anvil import *
+import anvil.js  # Import the anvil.js module
+
+class out_of_band_mgmt(out_of_band_mgmtTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+  def submit_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    self.output_card.visible = True
+    
+    oob_ip = self.ip_address.text
+    oob_subnet = self.subnet_mask.text
+    oob_gateway = self.default_gateway.text
+
+    self.output_text.text = f"""
+    dhcp-client client mgmtbr0 admin-enable false
+    oc-if:interfaces interface mgmtbr0 ipv4 addresses address { oob_ip } config ip { oob_ip } prefix-length {oob_subnet}
+    rib vrf default ipv4 0.0.0.0/0 next-hop { oob_gateway }
+    """
+
+  def copy_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    # Use anvil.js to call the browser's Clipboard API.
+    #    This is a JavaScript function being called directly from Python.
+    anvil.js.window.navigator.clipboard.writeText(self.output_text.text)
+
+    # (Optional) Provide feedback to the user.
+    self.copy_button.text = "Copied! 👍"
+    
+
+    
